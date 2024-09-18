347e89b7d0a12b44182f277545b0432179e9a642
yassinoss2001 <yassine.zitouni@esprit.tn>
2024-09-18 21:04:58 +0200

    init project

---
+++ Foyer/src/test/java/tn/esprit/foyer/FoyerApplicationTests.java
@@ -0,0 +1,13 @@
+package tn.esprit.foyer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoyerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
