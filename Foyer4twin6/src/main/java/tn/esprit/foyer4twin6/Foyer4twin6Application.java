6a7be2c085cac983e1d161620bf07db63f45b9b3
yassinoss2001 <yassine.zitouni@esprit.tn>
2024-09-19 11:02:52 +0200

    projet crée !

---
+++ Foyer4twin6/src/main/java/tn/esprit/foyer4twin6/Foyer4twin6Application.java
@@ -0,0 +1,13 @@
+package tn.esprit.foyer4twin6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Foyer4twin6Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Foyer4twin6Application.class, args);
+    }
+
+}
